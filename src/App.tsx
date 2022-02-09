6abb3d0133bb17244cda88d219db8ab4256ae6f4
Borna Pavlić <borna.pavlic@yahoo.com>
2022-02-09 15:47:49 +0100

    adding material ui and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div className='app'>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
