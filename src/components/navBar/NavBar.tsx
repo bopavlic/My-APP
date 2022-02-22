6887fc7c5344b8069a98007b2bf73b0761611a80
Borna Pavlić <borna.pavlic@yahoo.com>
2022-02-22 13:41:15 +0100

    added scrolling to top

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -12,18 +12,25 @@ import { useNavBarStyles } from './styled';
 
 const NavBar: React.FC<NavBarProps> = (props) => {
   const { darkMode, setDarkMode } = props;
+  const classes = useNavBarStyles();
 
   const handleModeSwitch = () => {
     setDarkMode((state) => !state);
   };
 
-  const classes = useNavBarStyles();
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='fixed'>
         <Toolbar className={classes.toolbar}>
           <IconButton
+            onClick={scrollToTop}
             size='large'
             edge='start'
             color='inherit'
@@ -32,7 +39,11 @@ const NavBar: React.FC<NavBarProps> = (props) => {
           >
             <CatchingPokemonIcon className={classes.navBar__icon} />
           </IconButton>
-          <Typography variant='h5' sx={{ flexGrow: 1 }}>
+          <Typography
+            onClick={scrollToTop}
+            variant='h5'
+            sx={{ flexGrow: 1, cursor: 'pointer' }}
+          >
             POKEMON
           </Typography>
           <FormGroup>
