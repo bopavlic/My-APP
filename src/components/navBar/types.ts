27ceeaf00789d725aad912c9406dd8fc47cf42dd
Borna Pavlić <borna.pavlic@yahoo.com>
2022-05-14 18:05:53 +0200

    feat - updated types

--- src/components/navBar/types.ts
+++
@@ -1,4 +0,0 @@
-export interface NavBarProps {
-  darkMode: boolean;
-  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-}
--- src/components/reduxPlayground/types.ts
+++
@@ -1,3 +0,0 @@
-export interface ReduxPlaygroundProps {
-  darkMode: boolean;
-}
