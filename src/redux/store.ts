aae1a2eb6348941eecc73472bbbdff2c08f40bf6
Borna Pavlić <borna.pavlic@yahoo.com>
2022-05-14 18:08:03 +0200

    added dark mode into redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import { dogsApi } from '../features/dogs/dogsApiSlice';
 import todoReducer from '../features/todos/todoSlice';
+import darkModeReducer from '../features/darkMode/darkModeSlice';
 
 export const store = configureStore({
   reducer: {
     [dogsApi.reducerPath]: dogsApi.reducer,
     counter: counterReducer,
     todo: todoReducer,
+    darkMode: darkModeReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(dogsApi.middleware);
